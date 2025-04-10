019cd442e649310116c86ac5135a48d14ec54d15
Ryan Rodriguez <ryancode2321@gmail.com>
2025-04-09 20:18:09 -0500

    Port Data

--- src/data/PortfolioData.jsx
+++ src/data/PortfolioData.jsx
@@ -1,55 +1,55 @@
-import CVS from "../assets/CVSpage.png";
-import TrueAuto from "../assets/TrueAutoAfter.jpg";
-import TribusWarranty from "../assets/TribusWarranty.jpg";
-import BoltOn from "../assets/BoltOnTechnology.jpg";
-import PDQ from "../assets/PDQ.jpg";
-import SNU from "../assets/SNU.jpg";
-import KippNorcal from "../assets/KippNorcal.jpg";
-import CAQH from "../assets/CAQH.jpg";
+import CVS from '../assets/CVSpage.png';
+import TrueAuto from '../assets/TrueAutoAfter.jpg';
+import TribusWarranty from '../assets/TribusWarranty.jpg';
+import BoltOn from '../assets/BoltOnTechnology.jpg';
+import PDQ from '../assets/PDQ.jpg';
+import SNU from '../assets/SNU.jpg';
+import KippNorcal from '../assets/KippNorcal.jpg';
+import CAQH from '../assets/CAQH.jpg';
 
 // Robert's Images
-import Pres1 from "../assets/Presentation1.png"
-import Pres2 from "../assets/Presentation2.png"
-import ConcertLanding from '../assets/ConcertLandingPage.png'
-import DesktopMobile from "../assets/DesktopMobile.png"
+import Pres1 from '../assets/Presentation1.png';
+import Pres2 from '../assets/Presentation2.png';
+import ConcertLanding from '../assets/ConcertLandingPage.png';
+import DesktopMobile from '../assets/DesktopMobile.png';
+import Flixx from '../assets/FlixxApp.jpg';
 
 const portfolioData = [
   {
-    title: "Flixx App",
+    title: 'Flixx App',
     description:
-      "This is an app that goes and displays details on your favorite moveis or tv shows. It gives out details on actors, budget cost, revenue, also who produce or direct them",
-    image: Pres1,
-    industry: "",
-    imageBefore: "",
-    link:"https://www.figma.com/design/UybSQFbPb36LGrzt2B0Gd1/E-commerce-Music-App?node-id=109575-2723&p=f&t=pKquGMI1dfo1tty1-0"
+      'This is an app that goes and displays details on your favorite moveis or tv shows. It gives out details on actors, budget cost, revenue, also who produce or direct them',
+    image: Flixx,
+    industry: '',
+    imageBefore: '',
+    link: '',
   },
 
   {
-    title: "Car dealership landing page",
-    description:
-      "This design is for a car dealership website and it's purpose is to provide a comprehensive online platform for potential car buyers to explore the dealership's inventory, learn about available services, and ultimately facilitate the process of purchasing a vehicle.",
+    title: 'Calorie Tracking App',
+    description: 'Track calories',
     image: Pres2,
-    industry: "",
-    imageBefore: "",
-    link:"https://www.figma.com/design/9ZlcwVyJzMzrYvtU2KRHX3/Car-Dealership-Landing-Page?node-id=0-1&p=f&t=qzpVNRcOyF0n4wtk-0"
+    industry: '',
+    imageBefore: '',
+    link: '',
   },
   {
-    title: "2024 Concert Landing Page",
+    title: '2024 Concert Landing Page',
     description:
-      "The purpose of this concert website design is to serve as a centralized online platform where users can find information about upcoming concerts, purchase tickets, and engage with content related to live music events.",
+      'The purpose of this concert website design is to serve as a centralized online platform where users can find information about upcoming concerts, purchase tickets, and engage with content related to live music events.',
     image: ConcertLanding,
-    industry: "",
-    imageBefore: "",
-    link:"https://www.figma.com/design/Gyynms4IEPCTSpPMvvZHKp/2024-Concert-Landing-Page?node-id=0-1&p=f&t=NATYhMyQaLaf6yCZ-0"
+    industry: '',
+    imageBefore: '',
+    link: 'https://www.figma.com/design/Gyynms4IEPCTSpPMvvZHKp/2024-Concert-Landing-Page?node-id=0-1&p=f&t=NATYhMyQaLaf6yCZ-0',
   },
   {
-    title: "E-commerce Web page & App",
+    title: 'E-commerce Web page & App',
     description:
-      "The purpose of this e-commerce shopping website & app is to facilitate the online buying and selling of goods and services while also offering a convenient and secure platform.",
+      'The purpose of this e-commerce shopping website & app is to facilitate the online buying and selling of goods and services while also offering a convenient and secure platform.',
     image: DesktopMobile,
-    industry: "",
-    imageBefore: "",
-    link:"https://www.figma.com/design/m2d7mo0bw55eSH7rGl50Y3/E-commerce-Web-%26-Mobile?node-id=0-1&p=f&t=wEIbeULDhiguBCHG-0"
+    industry: '',
+    imageBefore: '',
+    link: 'https://www.figma.com/design/m2d7mo0bw55eSH7rGl50Y3/E-commerce-Web-%26-Mobile?node-id=0-1&p=f&t=wEIbeULDhiguBCHG-0',
   },
 ];
 
