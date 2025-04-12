537119b2731b364882d24a699345a178123d4b8a
Ryan Rodriguez <ryancode2321@gmail.com>
2025-04-12 16:27:55 -0500

    fix/add resume

--- src/data/MenuData.jsx
+++ src/data/MenuData.jsx
@@ -1,16 +1,16 @@
 const headerMenu = [
-    {
-        text: "Portfolio",
-        link: "#portfolio"
-    },
-    {
-        text: "Skills",
-        link: "#skills"
-    },
-    {
-        text: "Download Resume",
-        link: "/robres.pdf"
-    }
-]
+  {
+    text: 'Portfolio',
+    link: '#portfolio',
+  },
+  {
+    text: 'Skills',
+    link: '#skills',
+  },
+  {
+    text: 'Download Resume',
+    link: '/RyanRes.pdf',
+  },
+];
 
 export default headerMenu;
