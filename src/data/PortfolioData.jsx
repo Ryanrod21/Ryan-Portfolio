951ee89326dce736a0a56bda22fc9a2187ef5968
Ryan Rodriguez <ryancode2321@gmail.com>
2025-04-16 21:22:32 -0500

    add links to webpage + description

--- src/data/PortfolioData.jsx
+++ src/data/PortfolioData.jsx
@@ -13,43 +13,46 @@ const portfolioData = [
     image: Flixx,
     industry: '',
     imageBefore: '',
-    link: '',
+    link: 'https://movie-app-six-kohl-38.vercel.app/',
   },
 
   {
     title: 'Basketball Training page',
-    description: 'Basketball',
+    description:
+      'Elevate your game with our basketball training platform—designed for players of all levels. Sign up for expert coaching, skill-building drills, and personalized programs to take your performance to the next level',
     image: BBall,
     industry: '',
     imageBefore: '',
-    link: '',
+    link: 'https://none-49164491.hubspotpagebuilder.com/basketball-test',
   },
 
   {
     title: 'Calorie Tracking App',
-    description: 'Track calories',
+    description:
+      'Stay on top of your nutrition with our easy-to-use calorie tracking app. Set goals, log meals, and track your progress to build healthier habits every day',
     image: Calorie,
     industry: '',
     imageBefore: '',
-    link: '',
+    link: 'https://calorie-tracker-beta-eight.vercel.app/',
   },
   {
     title: 'Running Attire',
-    description: '',
+    description:
+      'Gear up with the latest in running apparel, from performance shoes to moisture-wicking clothes and essential accessories. Whether you are hitting the trail or the pavement, find everything you need to run strong and stay comfortable',
     image: ShoeApp,
     industry: '',
     imageBefore: '',
-    link: '',
+    link: 'https://shoe-project-sand.vercel.app/',
   },
   {
     title: 'Porsche GT3 Info Page',
-    description: '',
+    description:
+      'Discover the thrill of the Porsche 911 GT3—where track-bred performance meets iconic design. Explore specs, features, and the engineering excellence that makes the GT3 a true driving machine',
     image: Porsche,
     industry: '',
     imageBefore: '',
-    link: '',
+    link: 'https://car-2-rho.vercel.app/',
   },
-  
 ];
 
 export default portfolioData;
--- src/sections/LogoSlider.jsx
+++ src/sections/LogoSlider.jsx
@@ -1,5 +1,5 @@
-import Slider from "react-slick";
-import { skillsData } from "../data/SkillsData";
+import Slider from 'react-slick';
+import { skillsData } from '../data/SkillsData';
 
 export default function LogoSlider() {
   const renderedLogos = skillsData.map((logo, index) => {
@@ -19,7 +19,7 @@ export default function LogoSlider() {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 0,
-    cssEase: "linear",
+    cssEase: 'linear',
     pauseOnHover: true,
     responsive: [
       {
@@ -36,10 +36,14 @@ export default function LogoSlider() {
   return (
     <div className="slider-container" id="skills">
       <div className="slider-content">
-      <h2>My Experience</h2>
-      <p>I've been designing websites for a year focusing on creating intuitive user experiences and visually appealing interfaces. My approach blends clean design principles with a user-centered mindset, ensuring that every website I build not only looks great but also functions seamlessly across devices.
-        Here are some of my skills:
-      </p>
+        <h2>My Experience</h2>
+        <p>
+          I've been designing websites for a year focusing on creating intuitive
+          user experiences and visually appealing interfaces. My approach blends
+          clean design principles with a user-centered mindset, ensuring that
+          every website I build not only looks great but also functions
+          seamlessly across devices. Here are some of my skills:
+        </p>
       </div>
       <Slider {...settings}>{renderedLogos}</Slider>
     </div>
