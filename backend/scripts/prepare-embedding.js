b80f747dc4b8bc0af7f007180b8c4ec68562b3f4
Ryan <ryancode2321@gmail.com>
2026-03-02 11:39:23 -0600

    updates

--- backend/scripts/prepare-embedding.js
+++ backend/scripts/prepare-embedding.js
@@ -12,7 +12,7 @@ const openai = new OpenAI({
 
 const supabase = createClient(
 	process.env.SUPABASE_URL,
-	process.env.SUPABASE_ANON_KEY,
+	process.env.SUPABASE_SERVICE_ROLE_KEY,
 );
 
 function createPortfolioChunks() {
