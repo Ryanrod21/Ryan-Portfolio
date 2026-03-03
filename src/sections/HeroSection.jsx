f781b5e45c55fdd5bf1a128f5d41495051914203
Ryan <ryancode2321@gmail.com>
2026-03-03 08:41:32 -0600

    new pic

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,45 +1,44 @@
-import hiveimg from '../assets/hiveprofileimg.png/';
-import RobertHero from '../assets/robertHero.png';
-import Rypic from '../assets/ryanpic.jpeg';
+import hiveimg from "../assets/hiveprofileimg.png/";
+import Rypic from "../assets/ryan-port-pic.png";
 
-import './section.css';
+import "./section.css";
 
 function HeroSection() {
-  return (
-    <>
-      <div className="hero-container w-full ">
-        <div className="gradient-top"></div>
-        <div className="gradient-bottom"></div>
-        <span className="blur-span left"></span>
-        <span className="blur-span right"></span>
-        <div className="hero-container-inner">
-          <div className="content text-left  flex flex-col gap-[30px]">
-            <h1>
-              Hello World, <br></br>I'm<span> Ryan Rodriguez </span>
-              <br></br> <h1 className="email">officialryanrod@gmail.com </h1>
-            </h1>
+	return (
+		<>
+			<div className="hero-container w-full ">
+				<div className="gradient-top"></div>
+				<div className="gradient-bottom"></div>
+				<span className="blur-span left"></span>
+				<span className="blur-span right"></span>
+				<div className="hero-container-inner">
+					<div className="content text-left  flex flex-col gap-[30px]">
+						<h1>
+							Hello World, <br></br>I'm<span> Ryan Rodriguez </span>
+							<br></br> <h1 className="email">officialryanrod@gmail.com </h1>
+						</h1>
 
-            <p>Creative web developer based in Robstown, TX</p>
-            <div className="button-wrap">
-              <a
-                href="Ryan Rodriguez Frontend Developer Resume.pdf"
-                className="primary-button"
-              >
-                Download Resume
-              </a>
-            </div>
-          </div>
-          <div className="img flex items-center justify-center">
-            <div className="img-label">
-              <span>Web Developer</span>
-            </div>
-            <div className="img-circle"></div>
-            <img src={Rypic} className="Rypic" />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+						<p>Creative web developer based in Robstown, TX</p>
+						<div className="button-wrap">
+							<a
+								href="Ryan Rodriguez Frontend Developer Resume.pdf"
+								className="primary-button"
+							>
+								Download Resume
+							</a>
+						</div>
+					</div>
+					<div className="img flex items-center justify-center">
+						<div className="img-label">
+							<span>Web Developer</span>
+						</div>
+						<div className="img-circle"></div>
+						<img src={Rypic} className="Rypic" />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default HeroSection;
